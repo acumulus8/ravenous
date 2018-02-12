05aae6e1c6466302f7508340d6ac87eb13310e59
a_cumulus <timwilburn8@gmail.com>
2018-02-12 13:23:32 -0500

    add minor changes

--- src/App.js
+++ src/App.js
@@ -11,7 +11,9 @@ class App extends React.Component {
     this.searchYelp = this.searchYelp.bind(this);
   }
   searchYelp(term, location, sortBy) {
-    Yelp.search(term, location, sortBy).then(businesses => this.setState({businesses: businesses}));
+    Yelp.search(term, location, sortBy).then(businesses => {
+      this.setState({businesses: businesses})
+    })
   }
 
   render() {
