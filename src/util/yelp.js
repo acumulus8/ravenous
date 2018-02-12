a22450679983ef66cb1f510507864e713c4dbb4e
a_cumulus <timwilburn8@gmail.com>
2018-02-12 14:53:28 -0500

    fix url, app working

--- src/util/yelp.js
+++ src/util/yelp.js
@@ -1,13 +1,13 @@
 const apiKey = 'c98yc2l1Kk0n5B0HuFKu0rIJbrP6CmKKRJYXyM6cx6RhS7236ESUXuvp86JsXQTltM6SVBUZAGrr6j8_u3Tc5wjoxIM0w5_CEpkaIAFAzXCICNauSGXvjzBWuNJ9WnYx';
 
 let Yelp = {
-    search: function (term, location, sortBy) {return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/business/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+    search: function (term, location, sortBy) {return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
         {headers: {Authorization: `Bearer ${apiKey}`}
         }).then(response => {
         return response.json();
         }).then(jsonResponse => {
             if (jsonResponse.businesses) {
-                console.log(businesses);
+                
                 return jsonResponse.businesses.map(business => ({
                         
                         id: business.id,
@@ -17,7 +17,7 @@ let Yelp = {
                         city: business.location.city,
                         state: business.locationstate,
                         zipCode: business.location.zip_code,
-                        category: business.category[0].title,
+                        category: business.categories[0].title,
                         rating: business.rating,
                         reviewCount: business.review_ount
                     
